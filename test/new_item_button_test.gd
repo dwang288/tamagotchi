60603523f2568857a0b039b986ed6299b8747503
Duan <duan.wang93@gmail.com>
2025-03-21 00:44:47 -0400

    added chicken breast

--- test/new_item_button_test.gd
+++ test/new_item_button_test.gd
@@ -1,9 +1,11 @@
 extends Button
 
+@export var items: Array[InventoryItemResource]
+
 signal sent_item(item: InventoryItemResource)
 # Called when the node enters the scene tree for the first time.
 
-
 func _on_pressed():
-	var item = load("res://items/itemResources/candy.tres")
-	sent_item.emit(item)
+	if items.size() > 0:
+		var item = items.pick_random()
+		sent_item.emit(item)
